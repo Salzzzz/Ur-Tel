ee09c2b64eeb994c6d5b9bdd55f6c89071b3ae17
da best <rickgolfs@proton.me>
2024-10-20 01:57:31 -0400

    first javascript. starting to understand it, but would like to look into parameters and variables

---
+++ let myVariabl;.js
@@ -0,0 +1,13 @@
+let myVariable = 123;
+const MY_VARIABLE = 123;
+let mystring1 ="hello"
+let mystring2 = "world";
+mystring1 + mystring2 + "1";
+function displayGreeting(name, salutation= 'your an asshole') {
+    console.log(`${salutation},${name}`);
+  }
+  function creategreetingmessage(name) {
+    const message = `hello, ${name}`;
+    return message;
+  }
+ 
