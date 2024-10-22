7ea4247101e97c794356d6f79c61d24815a49e6b
da best <rickgolfs@proton.me>
2024-10-22 15:42:28 -0400

    alot to take in today, review this before starting again later

--- let myVariabl;.js
+++ let myVariabl;.js
@@ -6,9 +6,37 @@ mystring1 + mystring2 + "1";
 function displayGreeting(name, salutation= 'your an asshole') {
     console.log(`${salutation},${name}`);
         const message = `hello, ${name}`;
+        console.log(message);
   }
  function createGreetingMessage(name) {
     const message = `Hello, ${name}`; 
-    return message;  
+    console.log(message) 
  }
-const greetingmessage = createGreetingMessage('rick');
+createGreetingMessage('rick');
+createGreetingMessage('birb');
+
+let seconds= 0;
+function displayDone() {
+    console.log(seconds + ' second/s has elapsed');
+}
+
+setTimeout(displayDone, seconds *1000);
+
+
+let currentMoney= 200;
+let laptopPrice= 500;
+let laptopdiscountPrice= laptopPrice - laptopPrice * 0.2; //laptop price at 20 percent off
+let difference= laptopdiscountPrice-currentMoney ;
+if (currentMoney >= laptopPrice || currentMoney >= laptopdiscountPrice) {
+    console.log("getting a new laptop!")}
+else {
+    console.log("no laptop you still need $" + difference + " to get that shit" );
+}
+if (!difference <= currentMoney) {
+    console.log("your so goddamn broke")
+} else {
+    console.log("your rich bitch");
+}
+let firstNumber = 20;
+let secondnumber = 10;
+let biggestnumber = firstNumber> secondnumber ? firstNumber : secondnumber;
